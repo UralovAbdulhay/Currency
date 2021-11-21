3c313f8f1e3ed9aa7bd10bc66d39f8fa7e020684
Abdulhay <71259096+UralovAbdulhay@users.noreply.github.com>
2021-11-21 19:14:39 +0500

    Update CommandLineAppStartupRunner.java

--- src/main/java/uz/abdulhay/currency/service/CommandLineAppStartupRunner.java
+++ src/main/java/uz/abdulhay/currency/service/CommandLineAppStartupRunner.java
@@ -24,10 +24,7 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     @Scheduled(cron = "0 1 * * * *")
     public void synchronizeData() {
 
-//          System.out.println(DateTimeFormatter.ofPattern("HH:mm:ss SSS").format(LocalTime.now()));
-//           System.out.println(DateTimeFormatter.ofPattern("HH:mm:ss SSS").format(LocalTime.now()));
         currencyService.synchronizeData();
-//          System.out.println(DateTimeFormatter.ofPattern("HH:mm:ss SSS").format(LocalTime.now())); 
     }
 
 
